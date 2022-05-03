2d3a9864449b8fb6a0997bb0859144280bb878ad
Shivesh Pratap Mall <shiveshmall12499@gmail.com>
2022-05-04 00:23:05 +0530

    summarizer added

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import time
 
+import numpy as np
+import pandas as pd
+import nltk
+nltk.download('punkt') # one time execution
+nltk.download('stopwords')
+import re 
+from nltk.tokenize import sent_tokenize
 r = sr.Recognizer()
 
 async def get_large_audio_transcription(path):
@@ -91,6 +98,128 @@ async def modelCall(file):
     sound = AudioSegment.from_mp3('static/uploads/AIaudio.mp3')
     sound.export("static/uploads/result.wav", format="wav")
     full_text=await get_large_audio_transcription("static/uploads/result.wav")
+    f1=open("Transcript.txt","w+")
+    f1.write(full_text)
+    full_text
+    text_lst=full_text.split(".")
+    sentences = []
+    for s in text_lst:
+        sentences.append(sent_tokenize(s))
+    sentences = [y for x in sentences for y in x]
+    word_embeddings = {}
+    f = open('glove.6B.100d.txt', encoding='utf-8')
+    for line in f:
+        values = line.split()
+        word = values[0]
+        coefs = np.asarray(values[1:], dtype='float32')
+        word_embeddings[word] = coefs
+    f.close()
+
+
+    # In[68]:
+
+
+    clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ",regex=True)
+
+    # make alphabets lowercase
+    clean_sentences = [s.lower() for s in clean_sentences]
+
+
+    # In[69]:
+
+
+    from nltk.corpus import stopwords
+
+
+    # In[70]:
+
+
+    stop_words = stopwords.words('english')
+    def remove_stopwords(sen):
+        sen_new = " ".join([i for i in sen if i not in stop_words])
+        return sen_new
+    clean_sentences = [remove_stopwords(r.split()) for r in clean_sentences]
+    clean_sentences
+
+
+    # In[71]:
+
+
+    sentence_vectors = []
+    for i in clean_sentences:
+        if (len(i) != 0):
+            v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()])/(len(i.split())+0.001)
+        else:
+            v = np.zeros((100,))
+        sentence_vectors.append(v)
+    sentence_vectors
+
+
+    # In[72]:
+
+
+    # similarity matrix
+    sim_mat = np.zeros([len(sentences), len(sentences)])
+    sim_mat
+
+
+    # In[73]:
+
+
+    from sklearn.metrics.pairwise import cosine_similarity
+
+
+    # In[74]:
+
+
+    for i in range(len(sentences)):
+        for j in range(len(sentences)):
+            if i != j:
+                sim_mat[i][j] = cosine_similarity(sentence_vectors[i].reshape(1,100), sentence_vectors[j].reshape(1,100))[0,0]
+
+
+    # In[75]:
+
+
+    sim_mat
+
+
+    # In[76]:
+
+
+    import networkx as nx
+
+    nx_graph = nx.from_numpy_array(sim_mat)
+    scores = nx.pagerank(nx_graph)
+
+
+    # In[77]:
+
+
+    ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
+
+
+    # In[78]:
+
+    sumy=""
+    for i in range(10):
+        sumy+=ranked_sentences[i][1]
+        print(ranked_sentences[i][1])
+
+
+    # In[ ]:
+
+
+
+
+
+
+
+
+    
+    return sumy
+
+
     return full_text
     
 
